1f4027b9ae9a721f4c4c55499f8695def5f2177e
Daniel Rochetti <daniel.rochetti@gmail.com>
2011-01-27 01:03:37 -0200

    string matcher

---
+++ src/main/java/org/brushingbits/jnap/common/util/StringMatcher.java
@@ -0,0 +1,46 @@
+/*
+ * StringMatcher.java created on 2010-10-26
+ *
+ * Created by Brushing Bits Labs
+ * http://www.brushingbits.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.brushingbits.jnap.common.util;
+
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
+
+/**
+ * @author Daniel Rochetti
+ * @since 1.0
+ */
+public final class StringMatcher {
+
+	private static PathMatcher matcher;
+	static {
+		matcher = new AntPathMatcher();
+	}
+	
+	public static boolean match(String value, String... patterns) {
+		boolean matched = false;
+		for (String pattern : patterns) {
+			matched = matcher.match(pattern, value);
+			if (matched) {
+				break;
+			}
+		}
+		return matched;
+	}
+
+}
